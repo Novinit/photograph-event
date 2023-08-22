db58263f250d125a595a6e63b90d92846ab94132
Johan Ghys <novinitwebdesign@gmail.com>
2023-08-22 16:23:38 +0200

    yyyy

--- home.php
+++ home.php
@@ -2,10 +2,11 @@
 
 
 <section  class="sliderpicture sectionblocks">
-    <img src="http://photographe-event.local/wp-content/uploads/2023/08/Photograhe-event-ambiance.jpg" alt="Photographe event ambiance">
     <div class="slidetitle">
         <h1>PHOTOGRAPHE EVENT</h1>
     </div>
+    <img src="http://photographe-event.local/wp-content/uploads/2023/08/Photograhe-event-ambiance.jpg"  alt="Photographe event ambiance">
+   
 </section>
 
 
