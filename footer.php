625f60503c671fbc3dbd2a3bee2397279fdbbc95
Johan Ghys <novinitwebdesign@gmail.com>
2023-09-20 15:39:33 +0200

    Release 1.1

--- footer.php
+++ footer.php
@@ -1,60 +1,17 @@
 
 
+
+
+<footer id="footerbox" class="sectionblocks">
+ 
+  <nav class="footermenu">
+    <?php wp_nav_menu( array('menu' => "Footer menu") ); ?>
+  <nav>
+
+</footer>
+ 
+<?php get_template_part('/templates/contact-modale'); ?>
 <?php wp_footer() ?>
-
-  <footer id="footerbox" class="sectionblocks">
- 
-    <nav class="footermenu">
-      <?php wp_nav_menu( array('menu' => "Footer menu") ); ?>
-    <nav>
-
-  
-
-  
-    
-
-
-
-
-
-
-  </footer>
- 
-<!-- Modal content -->
-<div id="myModal" class="modal">
-      
-      <div class="modal-content">
-        <div class="modal-header"></div>
-        <div class="close_popup"><span>&#215;</span></div>
-        <?php echo do_shortcode( '[contact-form-7 id="ab02557" title="Contact form 1"]') ?>
-      </div>
-</div>
-
-
-
-  
-
-
-<script>
-
-
-
-
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 </body>
 </html>
---
+++ templates/contact-modale.php
@@ -0,0 +1,12 @@
+
+
+<!-- Modal content -->
+
+
+<div id="myModal" class="modal">
+  <div class="modal-content">
+    <div class="modal-header"></div>
+    <div class="close_popup"><span>&#215;</span></div>
+    <?php echo do_shortcode( '[contact-form-7 id="ab02557" title="Contact form 1"]') ?>
+  </div>
+</div>
